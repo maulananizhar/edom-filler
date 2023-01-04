591706c548da4196f7f86c6ba3986a2770fb8a47
maulananizhar <maulananizhar27@gmail.com>
2023-01-04 14:59:25 +0700

    Fix Issue

--- bundle.js
+++ bundle.js
@@ -10,7 +10,29 @@ function createVariables()
     }
 }
 createVariables();
-    // let nilai1 = document.querySelectorAll("#nilai_1");
 
-    // nilai1[2].checked = true;
-// }
+nilai1[2].checked = true
+nilai2[1].checked = true
+nilai3[2].checked = true
+nilai4[1].checked = true
+nilai5[0].checked = true
+nilai6[3].checked = true
+nilai7[1].checked = true
+nilai8[3].checked = true
+nilai9[1].checked = true
+nilai10[3].checked = true
+nilai11[0].checked = true
+nilai12[0].checked = true
+nilai13[2].checked = true
+nilai14[1].checked = true
+nilai15[0].checked = true
+nilai16[2].checked = true
+nilai17[3].checked = true
+nilai18[3].checked = true
+nilai19[2].checked = true
+nilai20[2].checked = true
+nilai21[2].checked = true
+nilai22[0].checked = true
+nilai23[0].checked = true
+nilai24[1].checked = true
+nilai25[0].checked = true
